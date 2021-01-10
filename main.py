ee5e1a3a06667283ca9167722e80c3cfc5969992
Nevan Padayachee <padayacheenevan067@gmail.com>
2021-01-10 15:14:37 +0200

    Added user choice and confirmation

--- main.py
+++ main.py
@@ -40,10 +40,48 @@ test_loss, test_acc= model.evaluate(test_images,test_labels, verbose=1)
 
 print('Test accuracy: ', test_acc)
 
+#prediction = model.predict(test_images)
+#print(class_names[np.argmax(prediction[8])])
+#plt.figure()
+#plt.imshow(test_images[8])
+#plt.colorbar()
+#plt.grid(False)
+#plt.show()
+
+COLOR = 'white'
+plt.rcParams['text.color']=COLOR
+plt.rcParams['axes.labelcolor']=COLOR
+
+def get_number():
+    while True:
+        num = input("pick a number")
+        if num.isdigit():
+            num=int(num)
+            if 0<=num<=1000:
+                return num
+            else :
+                print("try again...")
+
+def predict(model, image, correct_label):
+    class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
+                   'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
+    prediction = model.predict(np.array([image]))
+    predicted_class=class_names[np.argmax(prediction)]
+    show_image(image,class_names[correct_label],predicted_class)
+
+
+def show_image(img, label, guess):
+    plt.figure()
+    plt.imshow(image, cmap=plt.cm.binary)
+    plt.colorbar()
+    plt.grid(False)
+    plt.show()
+
+
+num=get_number()
+image=test_images[num]
+label=test_labels[num]
+predict(model, image, label)
+print("Expected : "+class_names[test_labels[num]])
 prediction = model.predict(test_images)
-print(class_names[np.argmax(prediction[8])])
-plt.figure()
-plt.imshow(test_images[8])
-plt.colorbar()
-plt.grid(False)
-plt.show()
+print("Guess : "+class_names[np.argmax(prediction[num])])
