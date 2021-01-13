7f67ed32f262574c30951bf56c7d656a4e6bc6fb
Nevan Padayachee <padayacheenevan067@gmail.com>
2021-01-13 16:24:02 +0200

    added save of model and loaded model

--- main2.py
+++ main2.py
@@ -9,44 +9,10 @@ fashion_mnist = keras.datasets.fashion_mnist
 #splits into training and testing
 (train_images, train_labels), (test_images, test_labels)=fashion_mnist.load_data()
 
-#print(train_images.shape)
-
-#looking at the greyscale of one pixel [image_number,pixel,pixel]
-#print(train_images[0, 23, 23])
-
 class_names = ['T-shirt/top', 'Trouser', 'Pullover', 'Dress', 'Coat',
                'Sandal', 'Shirt', 'Sneaker', 'Bag', 'Ankle boot']
-#looking at one of the images
-#plt.figure()
-#plt.imshow(train_images[8])
-#plt.colorbar()
-#plt.grid(False)
-#plt.show()
 
-train_images = train_images/255.0
-test_images = test_images/255.0
-
-model = keras.Sequential([
-    keras.layers.Flatten(input_shape=(28, 28)),#layer1 injput
-    keras.layers.Dense(128, activation='relu'),#layer2 hidden
-    keras.layers.Dense(10, activation='softmax')#layer3 output
-])
-
-model.compile(optimizer='Adam', loss='sparse_categorical_crossentropy', metrics= ['accuracy'])
-
-model.fit(train_images, train_labels, epochs=10)
-
-test_loss, test_acc= model.evaluate(test_images,test_labels, verbose=1)
-
-print('Test accuracy: ', test_acc)
-
-#prediction = model.predict(test_images)
-#print(class_names[np.argmax(prediction[8])])
-#plt.figure()
-#plt.imshow(test_images[8])
-#plt.colorbar()
-#plt.grid(False)
-#plt.show()
+model=keras.models.load_model('models/firstsave.h5')
 
 COLOR = 'white'
 plt.rcParams['text.color']=COLOR
@@ -85,5 +51,3 @@ predict(model, image, label)
 print("Expected : "+class_names[test_labels[num]])
 prediction = model.predict(test_images)
 print("Guess : "+class_names[np.argmax(prediction[num])])
-
-model.save('models/firstsave.h5')
